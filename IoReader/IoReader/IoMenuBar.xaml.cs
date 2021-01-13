58a837bcb41ed081efc1509fbe2ef500f23b2414
andrii-itdev <andrii.itdev@gmail.com>
2021-01-13 20:30:12 +0200

    Added styles and templates for window, buttons and menues; implemented general view of the reader

---
+++ IoReader/IoReader/IoMenuBar.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IoReader
+{
+    /// <summary>
+    /// Interaction logic for IoMenuBar.xaml
+    /// </summary>
+    public partial class IoMenuBar : UserControl, INotifyPropertyChanged
+    {
+        public IoMenuBar()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty RevealConditionProperty = DependencyProperty.Register(
+            "RevealCondition", typeof(bool), typeof(IoMenuBar));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool RevealCondition
+        {
+            get { return (bool)GetValue(RevealConditionProperty); }
+            set
+            {
+                SetValue(RevealConditionProperty, value);
+                NotifyPropertyChanged();
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- IoReader/IoReader/MainWindow.xaml.cs
+++ IoReader/IoReader/MainWindow.xaml.cs
@@ -20,9 +20,77 @@ namespace IoReader
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ICommand _maximizeWindowCommand;
+        private ICommand _minimizeWindowCommand;
+        private ICommand _normlizeWindowCommand;
+        private ICommand _exitApplicationCommand;
+        private ICommand _dragMoveCommand;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _maximizeWindowCommand = new RelayCommand(OnMaximizeWindowExecute);
+            _minimizeWindowCommand = new RelayCommand(OnMinimizeWindowExecute);
+            _normlizeWindowCommand = new RelayCommand(OnNormlizeWindowExecute);
+            _exitApplicationCommand = new RelayCommand(OnExitApplicationExecute);
+            _dragMoveCommand = new RelayCommand(OnDragMoveExecute);
         }
+
+
+        public ICommand MaximizeWindowCommand
+        {
+            get { return _maximizeWindowCommand; }
+        }
+        public ICommand MinimizeWindowCommand
+        {
+            get { return _minimizeWindowCommand; }
+        }
+        public ICommand NormalizeWindowCommand
+        {
+            get { return _normlizeWindowCommand; }
+        }
+        public  ICommand ExitApplicationCommand
+        {
+            get { return _exitApplicationCommand; }
+        }
+        public ICommand DragMoveCommand
+        {
+            get { return _dragMoveCommand; }
+        }
+
+
+
+        private void OnMaximizeWindowExecute(object parameter)
+        {
+            if (Application.Current.MainWindow.WindowState == WindowState.Normal)
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            else NormalizeWindowCommand.Execute(parameter);
+            MenuBar.RevealCondition = true;
+        }
+
+        private void OnMinimizeWindowExecute(object parameter)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void OnNormlizeWindowExecute(object parameter)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
+
+        private void OnExitApplicationExecute(object parameter)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OnDragMoveExecute(object parameter)
+        {
+            //if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+            //    NormalizeWindowCommand.Execute(parameter);
+
+            DragMove();
+        }
+
     }
 }
